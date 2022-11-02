f708a4be271b9ac927a19cc283b54501887b0d91
raphael messian <raphael.messian@swirldslabs.com>
2022-11-02 14:59:06 +0100

    add SCRewardContract and SafeHTS

--- contracts/Rewards/SCRewards.sol
+++ contracts/Rewards/SCRewards.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8;
 
-import "../common/hedera/HederaTokenService.sol";
-import "../common/hedera/HederaResponseCodes.sol";
+import "../common/safe-HTS/HederaResponseCodes.sol";
 import "../common/IERC20.sol";
+import "../common/safe-HTS/SafeHTS.sol";
 
-contract scReward is HederaTokenService {
+contract scReward  {
 
     IERC20 public stakingToken;
     IERC20  public rewardsToken;
@@ -18,8 +18,8 @@ contract scReward is HederaTokenService {
         uint last_claimed_amount;
     }
 
-    uint256 public totalTokens;
-    uint256 public totalRewards;
+    uint public totalTokens;
+    uint public totalRewards;
 
     struct UserContributor {
         UserInfo userInfo;
@@ -32,30 +32,42 @@ contract scReward is HederaTokenService {
 
     mapping (address => UserInfo) public userContribution;
 
-    function addAccount(uint _numTokens) public {
-        userInfo = UserInfo(_numTokens, 0);
-        userContribution[msg.sender] = userInfo;
-        totalTokens += _numTokens;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
     }
 
-    function addReward(uint _amount) public {
-        uint256 factor = 1e12;
-        perShareRewards = ((_amount*factor) / totalTokens) % factor;
+    function initialize(address _stakingToken, address _rewardsToken) external onlyOwner{
+        SafeHTS.safeAssociateToken(_stakingToken, address(this));
+        SafeHTS.safeAssociateToken(_rewardsToken, address(this));
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardsToken);
+    }
+
+    function addAccount(address sender, uint amount) external {
+        SafeHTS.safeTransferToken(address(stakingToken), sender, sender, amount);
+        userInfo = UserInfo(amount, 0);
+        userContribution[msg.sender] = userInfo;
+        totalTokens += amount;
+    }
+
+    function addReward(uint _amount) external onlyOwner{
+        perShareRewards = (_amount / totalTokens)*1e18;
         totalRewards += perShareRewards;
     }
     
     function claimReward(address addr) public returns (uint){
-        reward = (totalRewards - userContribution[addr].last_claimed_amount) * userContribution[addr].num_shares;
+        reward = ((totalRewards - userContribution[addr].last_claimed_amount) * userContribution[addr].num_shares)/1e18;
         userContribution[addr].last_claimed_amount = totalRewards;
         return reward;
     }
 
-    function transfer(address a, address b, uint _amount) public {
-        uint rewardA = claimReward(a);
-        uint rewardB = claimReward(b);
-        userContribution[a].num_shares -= _amount;
-        userContribution[b].num_shares += _amount;
-    }
+    // function transfer(address a, address b, uint _amount) public {
+    //     uint rewardA = claimReward(a);
+    //     uint rewardB = claimReward(b);
+    //     userContribution[a].num_shares -= _amount;
+    //     userContribution[b].num_shares += _amount;
+    // }
     // function mint(uint _amount) public {
     //     reward = claimReward();
     //     totalTokens += _amount;
